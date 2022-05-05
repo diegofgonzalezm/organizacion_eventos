a0130ebcf92addfbf1ad49953dab9fa0462d73b1
diegofgonzalezm <104658742+diegofgonzalezm@users.noreply.github.com>
2022-05-04 21:04:56 -0500

    (11 commit)

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once("vista/index.html" ); ?>
+<?php include("templates/header.php"); ?>
+<header><link rel="stylesheet" href="css/index.css"></header>
+
+<section class="tefecto">
+    <video src="image/fondo.mp4" autoplay loop muted></video>
+    <h1 class="titulo">EVENTOS</h1>
+</section>
+<img src="image/banner.jpg" class="img-fluid" alt="Responsive image">
